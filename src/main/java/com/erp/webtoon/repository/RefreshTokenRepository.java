57e497c4a7bbf9e3f613abe1be7b7f72e2eebdaa
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-08 01:21:08 +0900

    FEAT : redis 접목을 위해 레포지토리 수

--- src/main/java/com/erp/webtoon/repository/RefreshTokenRepository.java
+++ src/main/java/com/erp/webtoon/repository/RefreshTokenRepository.java
@@ -1,13 +1,13 @@
 package com.erp.webtoon.repository;
 
 import com.erp.webtoon.domain.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
     boolean existsByEmail(String keyUserEmail);
     void deleteByEmail(String keyUserEmail);
