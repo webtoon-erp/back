1c5abc47f451cfa56802335b12ff3077bfb5a15b
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-08 01:24:07 +0900

    FEAT : 재발급 및 로그아웃 DTO 구현

---
+++ src/main/java/com/erp/webtoon/dto/token/TokenRequestDto.java
@@ -0,0 +1,18 @@
+package com.erp.webtoon.dto.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenRequestDto {
+    private String email;
+    private String accessToken;
+    private String refreshToken;
+}
