2bcc41d75033413d2cc74b00be7f568fa89a67f4
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-07-15 06:53:12 +0900

    FEAT : 검색 기능 구현

--- src/main/java/com/erp/webtoon/repository/UserRepository.java
+++ src/main/java/com/erp/webtoon/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.erp.webtoon.repository;
 
 import com.erp.webtoon.domain.User;
+import com.erp.webtoon.dto.user.UserListResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,4 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmployeeId(String employeeId);
     User findByLoginId(String employeeId);
     User findByEmail(String email);
+    List<UserListResponseDto> findAllByPositionContaining(String keyword);
 }
--- src/main/java/com/erp/webtoon/service/HrService.java
+++ src/main/java/com/erp/webtoon/service/HrService.java
@@ -36,4 +36,9 @@ public class HrService {
 
         return userList;
     }
+
+    public List<UserListResponseDto> searchPosts(String keyword) {
+        List<UserListResponseDto> userList = userRepository.findAllByPositionContaining(keyword);
+        return userList;
+    }
 }
