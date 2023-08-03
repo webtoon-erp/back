4b39459224fc51a2274833120151efe5a0744f0a
Gomson-E <gomsonixx@gmail.com>
2023-08-03 22:51:27 +0900

    FEAT : 출퇴근 등록 컨트롤러

---
+++ src/main/java/com/erp/webtoon/controller/AttendenceController.java
@@ -0,0 +1,28 @@
+package com.erp.webtoon.controller;
+
+import com.erp.webtoon.dto.attendece.AttendenceRequestDto;
+import com.erp.webtoon.dto.attendece.AttendenceResponseDto;
+import com.erp.webtoon.service.AttendenceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class AttendenceController {
+
+    private final AttendenceService attendenceService;
+
+    /*
+        출근 & 퇴근
+     */
+    @PostMapping("/attendence")
+    public void attendenceAdd(@RequestBody AttendenceRequestDto dto) throws IOException {
+        attendenceService.addAttendence(dto);
+    }
+
+}
