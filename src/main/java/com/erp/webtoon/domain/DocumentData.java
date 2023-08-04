cafe987c1605e95f9edaba7ad5f1c9c4bc6161a2
Gomson-E <gomsonixx@gmail.com>
2023-08-05 08:51:20 +0900

    REFACTOR : DocumnetData 도메인 정의

--- src/main/java/com/erp/webtoon/domain/DocumentData.java
+++ src/main/java/com/erp/webtoon/domain/DocumentData.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -13,8 +14,29 @@ public class DocumentData {
     @Column(name = "doc_data_id")
     private Long id;
 
-    private int sortSequence;   // 정렬 번호
-    private String content; // 내용
+    private LocalDateTime fromDate; // 시작일시
+
+    private LocalDateTime toDate;   // 종료일시
+
+    private String deptCode;        // 관련 부서코드
+
+    private String deptName;        // 관련 부서명
+
+    private String company;         // 관련 거래처
+
+    private String expenseType;     // 비용 타입
+
+    private int count;              // 수량
+
+    private int price;              // 단가
+
+    private int supAmt;             // 공급가액
+
+    private int vatAmt;             // 부가세액
+
+    private int totalAmt;           // 총금액
+
+    private String remark;          // 비고
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "doc_id")
