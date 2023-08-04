283efceea7323efcdd7b75ba2e38321922271c2f
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-04 23:21:19 +0900

    FEAT : 코멘트 조회 컨트롤러 구현

--- src/main/java/com/erp/webtoon/controller/RequestController.java
+++ src/main/java/com/erp/webtoon/controller/RequestController.java
@@ -2,15 +2,18 @@ package com.erp.webtoon.controller;
 
 import com.erp.webtoon.dto.itsm.RequestDto;
 import com.erp.webtoon.dto.itsm.RequestResponseDto;
+import com.erp.webtoon.dto.message.FeedbackListDto;
 import com.erp.webtoon.dto.message.MessageSaveDto;
 import com.erp.webtoon.service.RequestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -23,12 +26,18 @@ public class RequestController {
         return requestService.purchaseRequest(requestDto);
     }
 
-    @PostMapping("request")
+    @PostMapping("/request")
     public RequestResponseDto Request(@RequestBody RequestDto requestDto) throws Exception {
         return requestService.assistRequest(requestDto);
     }
 
-    @PostMapping void registerComment(@RequestBody MessageSaveDto messageSaveDto) throws IOException {
+    @PostMapping ("/comment")
+    public void registerComment(@RequestBody MessageSaveDto messageSaveDto) throws IOException {
         requestService.registerComment(messageSaveDto);
+    }
+
+    @GetMapping("/comment")
+    public List<FeedbackListDto> getAllComments(@RequestBody Long requestId){
+        return requestService.getAllComments(requestId);
     }
 }
