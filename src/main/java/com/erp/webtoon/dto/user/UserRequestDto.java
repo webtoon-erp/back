4755abb63fce58652e7ea763a1aa9cbd2bba0fc0
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-11 22:12:15 +0900

    feat : DTO에 validation 추가

--- src/main/java/com/erp/webtoon/dto/user/UserRequestDto.java
+++ src/main/java/com/erp/webtoon/dto/user/UserRequestDto.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
@@ -14,26 +16,37 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRequestDto {
+    @NotBlank
     private String employeeId; // 사번
 
+    @NotBlank
     private String password; // 비밀번호
 
+    @NotBlank
     private String name;    // 이름
 
+    @NotBlank
     private String email;   // 이메일
 
+    @NotBlank
     private String tel; // 전화번호
 
+    @NotNull
     private LocalDate birthDate;   // 생년월일
 
+    @NotBlank
     private String deptName;    // 부서명
 
+    @NotBlank
     private String deptCode; // 부서 코드
 
+    @NotNull
     private int teamNum;    // 팀 번호
 
+    @NotBlank
     private String position;    // 직급
 
+    @NotNull
     private LocalDate joinDate;    // 입사날짜
 
     private int dayOff;     // 연차개수
