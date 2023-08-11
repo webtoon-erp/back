75ff80c4831dd01dc68c6f0809b2d705a5cc17b1
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-11 20:41:55 +0900

    test : id로 요청 찾기 테스트

--- src/test/java/com/erp/webtoon/repository/RequestRepositoryTest.java
+++ src/test/java/com/erp/webtoon/repository/RequestRepositoryTest.java
@@ -2,8 +2,6 @@ package com.erp.webtoon.repository;
 
 import com.erp.webtoon.domain.Request;
 import com.erp.webtoon.domain.RequestDt;
-import com.erp.webtoon.fixture.RequestDtFixture;
-import com.erp.webtoon.fixture.RequestFixture;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 import static com.erp.webtoon.fixture.RequestDtFixture.*;
 import static com.erp.webtoon.fixture.RequestFixture.*;
+import static org.assertj.core.api.Assertions.*;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest(showSql = false)
@@ -34,7 +34,18 @@ class RequestRepositoryTest {
         Request save = requestRepository.save(request);
         List<Request> requestList = requestRepository.findAll();
         // then
-        Assertions.assertThat(requestList).contains(save);
-        Assertions.assertThat(request.getRequestDts().get(0)).isEqualTo(requestDt);
+        assertThat(requestList).contains(save);
+        assertThat(request.getRequestDts().get(0)).isEqualTo(requestDt);
+    }
+
+    @Test
+    @DisplayName("id로 요청 찾기 테스트")
+    void findById() {
+        // given
+        Request request = requestRepository.save(FIRST_REQUEST.생성(1L));
+        // when
+        Request request1 = requestRepository.findById(1L).orElseThrow(() -> new EntityNotFoundException("요청을 찾을 수 없습니다."));
+        // then
+        assertThat(request1).isEqualTo(request);
     }
 }
