99245636ba04906baf262894bb31c692ec9d35fd
Gomson-E <gomsonixx@gmail.com>
2023-08-12 05:47:53 +0900

    UPDATE : 메세지 조회 파라미터 변경

--- src/main/java/com/erp/webtoon/controller/MessageController.java
+++ src/main/java/com/erp/webtoon/controller/MessageController.java
@@ -1,16 +1,14 @@
 package com.erp.webtoon.controller;
 
-import com.erp.webtoon.domain.User;
-import com.erp.webtoon.dto.message.MessageFindDto;
 import com.erp.webtoon.dto.message.MessageListDto;
 import com.erp.webtoon.dto.message.MessageUpdateDto;
 import com.erp.webtoon.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -22,9 +20,9 @@ public class MessageController {
     /*
         메시지 조회
      */
-    @GetMapping("/message")
-    public List<MessageListDto> message(@RequestBody MessageFindDto dto) {
-        return messageService.getMessageList(dto);
+    @GetMapping("/message/{employeeId}")
+    public List<MessageListDto> message(@PathVariable("employeeId") String employeeId ) {
+        return messageService.getMessageList(employeeId);
     }
 
     /*
