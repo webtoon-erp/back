d7b586395aa544c3c7bdb5277813dd04bfbc7451
Gomson-E <gomsonixx@gmail.com>
2023-08-15 08:35:20 +0900

    CHORE : 미사용 코드 삭제

--- src/main/java/com/erp/webtoon/repository/UserRepository.java
+++ src/main/java/com/erp/webtoon/repository/UserRepository.java
@@ -1,10 +1,7 @@
 package com.erp.webtoon.repository;
 
 import com.erp.webtoon.domain.User;
-import com.erp.webtoon.dto.user.UserListResponseDto;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +14,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // 전체 직원 수 조회
     Long countAllBy();
 
-    // 전체 부서 리스트 조회
-    List<String> findDistinctDeptCodeBy();
 }
