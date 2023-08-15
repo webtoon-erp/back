03c8feefba1b4e713cfdc6cf8f08c4c95f96d3db
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-15 22:15:13 +0900

    CHORE : 필요 없어진 필드값 삭제

--- src/main/java/com/erp/webtoon/dto/itsm/RequestDtResponseDto.java
+++ src/main/java/com/erp/webtoon/dto/itsm/RequestDtResponseDto.java
@@ -6,8 +6,6 @@ import lombok.Data;
 @Data
 public class RequestDtResponseDto {
 
-    private int sortSequence; // 정렬번호
-
     private String content; // 상세 내용
 
     private int count;  // 수량
@@ -15,7 +13,6 @@ public class RequestDtResponseDto {
     private int cost;   // 가격
 
     public RequestDtResponseDto(RequestDt requestDt) {
-        this.sortSequence = requestDt.getSortSequence();
         this.content = requestDt.getContent();
         this.count = requestDt.getCount();
         this.cost = requestDt.getCost();
