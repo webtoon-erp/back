fed02dbc5d438a9db8c2492a2b13b873fd27c0c7
Gomson-E <gomsonixx@gmail.com>
2023-08-17 19:10:20 +0900

    FEAT : 전체 근태 현황 직원 리스트 DTO 추가

--- src/main/java/com/erp/webtoon/dto/attendece/TotalAttendenceSummaryDto.java
+++ src/main/java/com/erp/webtoon/dto/attendece/TotalAttendenceSummaryDto.java
@@ -9,16 +9,30 @@ public class TotalAttendenceSummaryDto {
 
     private Long totalUserCnt;          // 전체 직원수
 
+    private TotalAttendenceUserListDto totalUserList;       // 전체 직원 리스트
+
     private Long onTimeStartUserCnt;    // 정상 출근 직원수
+
+    private TotalAttendenceUserListDto onTimeStartUserList; // 정상 출근 직원 리스트
 
     private Long lateStartUserCnt;      // 지각 출근 직원수
 
+    private TotalAttendenceUserListDto lateStartUserList;   // 지각 출근 직원 리스트
+
     private Long notStartUserCnt;       // 미출근 직원수
+
+    private TotalAttendenceUserListDto notStartUserList;    // 미출근 직원 리스트
 
     private Long dayOffUserCnt;         // 휴가 직원수
 
+    private TotalAttendenceUserListDto dayOffUserList;      // 휴가 직원 리스트
+
     private Long onTimeEndUserCnt;      // 정상 퇴근 직원수
 
+    private TotalAttendenceUserListDto onTimeEndUserList;   // 정상 퇴근 직원 리스트
+
     private Long notEndUserCnt;         // 연장근무 직원수
+
+    private TotalAttendenceUserListDto notEndUserList;      // 연장근무 직원 리스트
 
 }
