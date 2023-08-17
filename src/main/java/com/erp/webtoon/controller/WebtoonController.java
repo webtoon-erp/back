23a74c37685ad46ccbae6e4c579cb0108b8e4581
lalabulla <dlrbgus722@naver.com>
2023-08-17 15:08:32 +0900

    FIX : Location URI 생성 코드 수정

--- src/main/java/com/erp/webtoon/controller/WebtoonController.java
+++ src/main/java/com/erp/webtoon/controller/WebtoonController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -122,7 +123,11 @@ public class WebtoonController {
         webtoonService.update(webtoonId, dto);
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(URI.create("/webtoon/{webtoonId}"));
+        URI location = UriComponentsBuilder.newInstance()
+                .path("/webtoon/{webtoonId}")
+                .buildAndExpand(webtoonId).toUri();
+
+        headers.setLocation(location);
 
         return new ResponseEntity(headers, HttpStatus.MOVED_PERMANENTLY);
     }
