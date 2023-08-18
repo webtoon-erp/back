ea434d0d3da66cfd5f7659e3570817b7e31da019
lalabulla <dlrbgus722@naver.com>
2023-08-19 00:14:42 +0900

    TEST : 웹툰 회차 등록 테스트

---
+++ src/test/java/com/erp/webtoon/controller/WebtoonDtControllerTest.java
@@ -0,0 +1,93 @@
+package com.erp.webtoon.controller;
+
+import com.erp.webtoon.domain.User;
+import com.erp.webtoon.domain.Webtoon;
+import com.erp.webtoon.domain.WebtoonDt;
+import com.erp.webtoon.dto.webtoon.WebtoonDtRequestDto;
+import com.erp.webtoon.repository.UserRepository;
+import com.erp.webtoon.repository.WebtoonDtRepository;
+import com.erp.webtoon.repository.WebtoonRepository;
+import com.erp.webtoon.service.WebtoonDtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class WebtoonDtControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private WebtoonRepository webtoonRepository;
+
+    @Autowired
+    private WebtoonDtService webtoonDtService;
+
+    @Autowired
+    private WebtoonDtRepository webtoonDtRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public MockMultipartFile getMultipartFile() throws IOException {
+        return new MockMultipartFile("file", "test.png", "image/png", new FileInputStream("/Users/kh/Desktop/file/파일명.png"));
+    }
+
+    @Test
+    @DisplayName("웹툰 회차 임시 업로드(최초 등록) - 파일O")
+    void test1() throws Exception {
+        //given
+        User newUser = User.builder()
+                .employeeId("20232023")
+                .name("규규")
+                .position("과장")
+                .build();
+
+        userRepository.save(newUser);
+
+        Webtoon newWebtoon = Webtoon.builder()
+                .title("웹툰입니다.")
+                .intro("인트로입니다.")
+                .artist("작가입니다.")
+                .build();
+
+        webtoonRepository.save(newWebtoon);
+
+        WebtoonDtRequestDto requestDto = new WebtoonDtRequestDto();
+        requestDto.setWebtoonId(newWebtoon.getId());
+        requestDto.setSubTitle("세상에 이런일이");
+        requestDto.setEmployeeId("20232023");
+
+        MockMultipartFile dto = new MockMultipartFile("dto", "dto", "application/json", objectMapper.writeValueAsString(requestDto).getBytes(StandardCharsets.UTF_8));
+
+        //expected
+        mockMvc.perform(multipart("/webtoonDt")
+                .file(dto)
+                .file(getMultipartFile()))
+                .andExpect(status().isMovedPermanently())
+                .andDo(print());
+
+    }
+
+}
---
+++ src/test/java/com/erp/webtoon/service/WebtoonDtServiceTest.java
@@ -0,0 +1,79 @@
+package com.erp.webtoon.service;
+
+import com.erp.webtoon.domain.User;
+import com.erp.webtoon.domain.Webtoon;
+import com.erp.webtoon.domain.WebtoonDt;
+import com.erp.webtoon.dto.webtoon.WebtoonDtRequestDto;
+import com.erp.webtoon.repository.UserRepository;
+import com.erp.webtoon.repository.WebtoonDtRepository;
+import com.erp.webtoon.repository.WebtoonRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class WebtoonDtServiceTest {
+
+    @Autowired
+    private WebtoonRepository webtoonRepository;
+
+    @Autowired
+    private WebtoonDtService webtoonDtService;
+
+    @Autowired
+    private WebtoonDtRepository webtoonDtRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public MockMultipartFile getMultipartFile() throws IOException {
+        return new MockMultipartFile("file", "test.png", "image/png", new FileInputStream("/Users/kh/Desktop/file/파일명.png"));
+    }
+
+    @Test
+    @DisplayName("웹툰 회차 임시 업로드(최초 등록) - 파일O")
+    void test1() throws IOException {
+        //given
+        User newUser = User.builder()
+                .employeeId("20232023")
+                .name("규규")
+                .position("과장")
+                .build();
+
+        userRepository.save(newUser);
+
+        Webtoon newWebtoon = Webtoon.builder()
+                .title("웹툰입니다.")
+                .intro("인트로입니다.")
+                .artist("작가입니다.")
+                .build();
+
+        webtoonRepository.save(newWebtoon);
+
+        WebtoonDtRequestDto requestDto = new WebtoonDtRequestDto();
+        requestDto.setWebtoonId(newWebtoon.getId());
+        requestDto.setSubTitle("세상에 이런일이");
+        requestDto.setEmployeeId("20232023");
+
+        //when
+        webtoonDtService.upload(requestDto, getMultipartFile());
+
+        //then
+        assertEquals(1L, webtoonDtRepository.count());
+        WebtoonDt webtoonDt = webtoonDtRepository.findAll().get(0);
+        assertEquals(1, webtoonDt.getEpisodeNum());
+        assertEquals("세상에 이런일이", webtoonDt.getSubTitle());
+        assertEquals("규규", webtoonDt.getManager());
+    }
+
+}
