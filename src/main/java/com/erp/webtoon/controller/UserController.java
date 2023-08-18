b296eabf1db4904b4a337d29d3bc2a1d38fd8ce5
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-08-18 22:23:44 +0900

    FEAT : 자격증 삭제 컨트롤러 구현

--- src/main/java/com/erp/webtoon/controller/UserController.java
+++ src/main/java/com/erp/webtoon/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -100,6 +101,15 @@ public class UserController {
     @PatchMapping
     public List<QualificationModifyResponseDto> modifyQualification(@RequestBody List<QualificationModifyRequestDto> qualificationModifyRequestDtoList) {
         return userService.updateQualification(qualificationModifyRequestDtoList);
+    }
+
+    /**
+     * 자격증 삭제
+     */
+    @DeleteMapping
+    public ResponseEntity deleteQualification(@RequestBody List<QualificationDeleteRequestDto> qualificationDeleteRequestDtoList) {
+        userService.deleteQualification(qualificationDeleteRequestDtoList);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
 
