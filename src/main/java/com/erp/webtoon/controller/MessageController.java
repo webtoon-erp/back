792a3e9d12d978fe78da74e3ec64e35eb40d7a93
Gomson-E <gomsonixx@gmail.com>
2023-08-22 16:02:05 +0900

    UPDATE : 메시지 컨트롤러 URL 매핑 수정

--- src/main/java/com/erp/webtoon/controller/MessageController.java
+++ src/main/java/com/erp/webtoon/controller/MessageController.java
@@ -4,32 +4,30 @@ import com.erp.webtoon.dto.message.MessageListDto;
 import com.erp.webtoon.dto.message.MessageUpdateDto;
 import com.erp.webtoon.service.MessageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/message")
 public class MessageController {
 
     private final MessageService messageService;
 
     /*
-        메시지 조회
+        개인 메시지 조회
      */
-    @GetMapping("/message/{employeeId}")
-    public List<MessageListDto> message(@PathVariable String employeeId) {
+    @GetMapping("/{employeeId}")
+    public List<MessageListDto> getMessageList(@PathVariable String employeeId) {
         return messageService.getMessageList(employeeId);
     }
 
     /*
         메시지 상태 변경
      */
-    @GetMapping("/message/updateStat")
-    public void messageModify(@RequestBody MessageUpdateDto dto) {
+    @PatchMapping("/updateStat")
+    public void updateMessageStatus(@RequestBody MessageUpdateDto dto) {
         messageService.modifyStat(dto);
     }
 
