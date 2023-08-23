6702528e0e1d80061835216a7845091340929226
Gomson-E <gomsonixx@gmail.com>
2023-08-24 00:32:02 +0900

    UPDATE : 예외 처리

--- src/main/java/com/erp/webtoon/controller/AttendanceController.java
+++ src/main/java/com/erp/webtoon/controller/AttendanceController.java
@@ -3,8 +3,10 @@ package com.erp.webtoon.controller;
 import com.erp.webtoon.dto.attendance.AttendanceRequestDto;
 import com.erp.webtoon.dto.attendance.AttendanceResponseDto;
 import com.erp.webtoon.dto.attendance.TotalAttendanceResponseDto;
+import com.erp.webtoon.dto.common.ErrorResponseDto;
 import com.erp.webtoon.service.AttendanceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -20,8 +22,14 @@ public class AttendanceController {
         출근 & 퇴근
      */
     @PostMapping
-    public void addAttendanceRecord(@RequestBody AttendanceRequestDto dto) throws IOException {
-        attendanceService.addAttendance(dto);
+    public ResponseEntity addAttendanceRecord(@RequestBody AttendanceRequestDto dto) throws IOException {
+        try {
+            attendanceService.addAttendance(dto);
+            return ResponseEntity.ok().build();
+        }
+        catch (IllegalStateException e) {
+            return ResponseEntity.badRequest().body(new ErrorResponseDto(e.getMessage()));
+        }
     }
 
     /*
