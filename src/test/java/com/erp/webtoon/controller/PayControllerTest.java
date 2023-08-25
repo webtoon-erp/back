585b8413097b407ca271fa82e2ce3bebf41da94a
lalabulla <dlrbgus722@naver.com>
2023-08-25 17:43:51 +0900

    TEST : 급여 개인 상세 조회 테스트

--- src/test/java/com/erp/webtoon/controller/PayControllerTest.java
+++ src/test/java/com/erp/webtoon/controller/PayControllerTest.java
@@ -1,8 +1,12 @@
 package com.erp.webtoon.controller;
 
+import com.erp.webtoon.domain.Qualification;
 import com.erp.webtoon.domain.User;
+import com.erp.webtoon.dto.pay.PayQualificationDto;
 import com.erp.webtoon.dto.pay.PayRequestDto;
+import com.erp.webtoon.dto.pay.PayResponseDto;
 import com.erp.webtoon.repository.UserRepository;
+import com.erp.webtoon.service.PayService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,11 +16,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -31,6 +39,9 @@ class PayControllerTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PayService payService;
 
     @Test
     @DisplayName("월 급여 등록")
@@ -57,5 +68,50 @@ class PayControllerTest {
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isMovedPermanently())
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("개인 급여 상세 조회")
+    void test2() throws Exception {
+        //given
+        User user = User.builder()
+                .employeeId("2000")
+                .name("규규")
+                .email("kkk@naver.com")
+                .deptName("인사과")
+                .build();
+
+        List<Qualification> qualList = new ArrayList<>();
+        Qualification qualification = Qualification.builder()
+                .qlfcType("정처기")
+                .content(null)
+                .qlfcDate(LocalDate.now())
+                .qlfcPay(50000)
+                .user(user)
+                .build();
+        qualList.add(qualification);
+        user.registerQualification(qualList);
+
+        userRepository.save(user);
+
+        PayRequestDto dto = new PayRequestDto();
+        dto.setEmployeeId("2000");
+        dto.setYearSalary(100000);
+        dto.setAddSalary(20000);
+        dto.setBankAccount("000-000-000-000");
+        dto.setPayDate(LocalDate.now());
+
+        payService.save(dto);
+
+        //expected
+        mockMvc.perform(get("/pays/{employeeId}", "2000")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userInfo.name").value("규규"))
+                .andExpect(jsonPath("$.userInfo.email").value("kkk@naver.com"))
+                .andExpect(jsonPath("$.monthPay.bankAccount").value("000-000-000-000"))
+                .andExpect(jsonPath("$.monthPay.monthSalary").value(8333))
+                .andExpect(jsonPath("$.monthPay.qualSalary").value(50000))
+                        .andDo(print());
     }
 }
--- src/test/java/com/erp/webtoon/service/PayServiceTest.java
+++ src/test/java/com/erp/webtoon/service/PayServiceTest.java
@@ -1,16 +1,23 @@
 package com.erp.webtoon.service;
 
 import com.erp.webtoon.domain.Pay;
+import com.erp.webtoon.domain.Qualification;
 import com.erp.webtoon.domain.User;
+import com.erp.webtoon.dto.pay.PayQualificationDto;
 import com.erp.webtoon.dto.pay.PayRequestDto;
+import com.erp.webtoon.dto.pay.PayResponseDto;
+import com.erp.webtoon.dto.user.QualificationRequestDto;
 import com.erp.webtoon.repository.PayRepository;
 import com.erp.webtoon.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,6 +31,15 @@ class PayServiceTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private  UserService userService;
+
+    @BeforeEach
+    void clean() {
+        userRepository.deleteAll();
+        payRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("월 급여 등록")
@@ -52,5 +68,54 @@ class PayServiceTest {
         assertEquals(100000, pay.getSalary());
         assertEquals("000-000-000-000", pay.getBankAccount());
         assertEquals(LocalDate.now(), pay.getPayDate());
+    }
+
+    @Test
+    @DisplayName("개인 급여 상세 조회")
+    void test2() {
+        //given
+        User user = User.builder()
+                .employeeId("2000")
+                .name("규규")
+                .email("kkk@naver.com")
+                .deptName("인사과")
+                .build();
+
+        List<Qualification> qualList = new ArrayList<>();
+        Qualification qualification = Qualification.builder()
+                .qlfcType("정처기")
+                .content(null)
+                .qlfcDate(LocalDate.now())
+                .qlfcPay(50000)
+                .user(user)
+                .build();
+        qualList.add(qualification);
+        user.registerQualification(qualList);
+
+        userRepository.save(user);
+
+        PayRequestDto dto = new PayRequestDto();
+        dto.setEmployeeId("2000");
+        dto.setYearSalary(100000);
+        dto.setAddSalary(20000);
+        dto.setBankAccount("000-000-000-000");
+        dto.setPayDate(LocalDate.now());
+
+        payService.save(dto);
+
+        //when
+        PayResponseDto searchDto = payService.search("2000");
+
+        //then
+        assertEquals("규규", searchDto.getUserInfo().getName());
+        assertEquals("kkk@naver.com", searchDto.getUserInfo().getEmail());
+        assertEquals("인사과", searchDto.getUserInfo().getDeptName());
+        assertEquals("000-000-000-000", searchDto.getMonthPay().getBankAccount());
+        assertEquals(100000, searchDto.getMonthPay().getYearSalary());
+        assertEquals(8333, searchDto.getMonthPay().getMonthSalary());
+        assertEquals(50000, searchDto.getMonthPay().getQualSalary());
+        PayQualificationDto payQualificationDto = searchDto.getQualificationList().get(0);
+        assertEquals("정처기", payQualificationDto.getName());
+        assertEquals(50000, payQualificationDto.getMoney());
     }
 }
