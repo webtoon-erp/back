8b22fb98af7338fd1ee7a04f51b029aa5a64c043
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-09-03 15:41:34 +0900

    FEAT : 로그인 시 사번과 직책 추가 반환

--- src/main/java/com/erp/webtoon/dto/token/TokenResponseDto.java
+++ src/main/java/com/erp/webtoon/dto/token/TokenResponseDto.java
@@ -1,5 +1,6 @@
 package com.erp.webtoon.dto.token;
 
+import com.erp.webtoon.domain.User;
 import com.erp.webtoon.token.TokenConst;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,11 +16,15 @@ import lombok.Setter;
 public class TokenResponseDto {
     private String grantType;
     private String accessToken;
+    private String employeeId;
+    private String position;
 
-    public static TokenResponseDto from(String accessToken) {
+    public static TokenResponseDto from(String accessToken, User user) {
         return TokenResponseDto.builder()
                 .grantType(TokenConst.TOKEN_PREFIX)
                 .accessToken(accessToken)
+                .employeeId(user.getEmployeeId())
+                .position(user.getPosition())
                 .build();
     }
 }
