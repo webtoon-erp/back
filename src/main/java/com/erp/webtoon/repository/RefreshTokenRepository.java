2a6fbd9a794fa2ea3beee488ab460915a72e47e8
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-09-16 01:34:58 +0900

    REFACTOR : redis에서 사번을 이용해 삭제할 수 있도록 설정

--- src/main/java/com/erp/webtoon/repository/RefreshTokenRepository.java
+++ src/main/java/com/erp/webtoon/repository/RefreshTokenRepository.java
@@ -4,4 +4,5 @@ import com.erp.webtoon.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+    void deleteByEmployeeId(String employeeId);
 }
