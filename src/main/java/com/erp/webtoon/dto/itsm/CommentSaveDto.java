4d6c8a11f948ec7cef298299f7e866017444ad40
Brilly-Bohyun <bohyun564@swu.ac.kr>
2023-09-16 01:46:15 +0900

    FEAT : 피드백 등록 dto 변경 및 구현

---
+++ src/main/java/com/erp/webtoon/dto/itsm/CommentSaveDto.java
@@ -0,0 +1,35 @@
+package com.erp.webtoon.dto.itsm;
+
+import com.erp.webtoon.domain.Message;
+import com.erp.webtoon.domain.User;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class CommentSaveDto {
+    @NotBlank
+    private String msgType;     // 메세지 타입
+
+    @NotBlank
+    private String content;     // 메세지 내용
+
+    @NotBlank
+    private Long refId; // 참조 ID
+
+    private String programId;    // 참조 프로그램ID
+
+    @NotBlank
+    private String sendEmpId;
+
+    public Message toEntity(User sendUser) {
+        return Message.builder()
+                .msgType(msgType)
+                .content(content)
+                .refId(refId)
+                .programId(programId)
+                .sendUser(sendUser)
+                .build();
+    }
+
+}
