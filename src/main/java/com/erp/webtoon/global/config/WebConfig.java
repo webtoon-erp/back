6afc71a211b8ad2c4164deb411645f2a91e1daeb
lalabulla <dlrbgus722@naver.com>
2023-10-08 23:05:26 +0900

    REFACTOR : 파일 이미지 불러오기

--- src/main/java/com/erp/webtoon/global/config/WebConfig.java
+++ src/main/java/com/erp/webtoon/global/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/image/**")
+        registry.addResourceHandler(resourcePath + "**")
                 .addResourceLocations("file:///" + resourcePath);
     }
 }
