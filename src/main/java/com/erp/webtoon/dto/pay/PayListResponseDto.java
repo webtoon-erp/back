129e45dc025ab92027d514dbcfb84fff95a991c2
lalabulla <dlrbgus722@naver.com>
2023-10-09 16:30:20 +0900

    REFACTOR : 급여 월 데이터 추가

--- src/main/java/com/erp/webtoon/dto/pay/PayListResponseDto.java
+++ src/main/java/com/erp/webtoon/dto/pay/PayListResponseDto.java
@@ -8,15 +8,15 @@ import java.time.LocalDate;
 @Data
 public class PayListResponseDto {
 
+    private String payMonth;   // 지급월
+
     private LocalDate payDate; // 지급날짜
 
     private int totalMonthSalary; // 월 지급 총합
 
-    private boolean payYN; // 지급여부
-
     public PayListResponseDto(Pay pay) {
+        this.payMonth = getPayDate().toString().substring(0, 4);
         this.totalMonthSalary = pay.getSalary() + pay.getAddPay();
         this.payDate = pay.getPayDate();
-        this.payYN = pay.isPayYN();
     }
 }
